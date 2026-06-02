4d2bf35071aae7d0a7898cf3f8f5f16ffd4b303c
groomigroom <googjohn0143@gmail.com>
2026-06-03 08:54:09 +0900

    Add files via upload

---
+++ calc_Y_slide/script.js
@@ -0,0 +1,10 @@
+let slide_train = document.querySelector(".slide_train");
+let count = 0;
+
+setInterval(function () {
+    count++;
+    if (count > 2) {
+        count = 0;
+    }
+    slide_train.style.transform = `translateY(${-400 * count}px)`;
+}, 2500)
